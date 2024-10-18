f5498f5910cb466d4f444adea8d988031d3cf07e
khoilvgcc210040 <khoilieuct03@gmail.com>
2024-10-18 16:15:34 +0700

    update step 1 signup ui

--- components/card-wrapper.tsx
+++ components/card-wrapper.tsx
@@ -21,9 +21,9 @@ export const CardWrapper = ({
   return (
     <div className="flex justify-center items-start bg-white">
       {/* Card Container */}
-      <div className="flex flex-col border-t border-l border-r p-6 max-w-md w-full bg-white" style={{ height: '695px' }}>
+      <div className="flex flex-col border max-w-md w-full bg-white min-h-[720px] sm:min-h-[695px] flex-grow" style={{ border: '2px solid #999' }}>
         {/* Header */}
-        <div className="flex justify-between items-center mb-4 border-b border-gray-400 pb-4">
+        <div className="flex justify-between items-center mb-4 border-b border-gray-400 p-6" style={{ borderBottom: '2px solid #999' }}>
           <Link href={backButtonHref} className="text-xl">
             {backButtonLabel}
           </Link>
@@ -32,21 +32,41 @@ export const CardWrapper = ({
         </div>
 
         {/* Avatar Image */}
-        <div className="flex justify-center mt-6 mb-6">
-          <img src="/images/logo.png" alt="avatar" className="w-20 h-20 object-contain" />
-        </div>
+        
 
-        {/* Card */}
-        <Card className="w-full">
+
+        <Card className="w-full ">
+          <div className="flex justify-center mt-2 mb-6">
+            <img src="/images/logo.png" alt="avatar" className="w-20 h-20 object-contain" />
+          </div>
           <CardContent>{children}</CardContent>
         </Card>
 
-        {/* Footer nằm trong khung */}
-        <footer className="flex justify-around w-full bg-gray-100 py-4 mt-2 rounded-lg">
-          <Link href="/" className="text-sm">Home</Link>
-          <Link href="/bookings" className="text-sm">Bookings</Link>
-          <Link href="/mydata" className="text-sm">My Data</Link>
+        <footer className="flex justify-around w-full py-4" style={{ marginTop: 'auto', borderTop: '2px solid #999' }}>
+          <Link href="/" className="nav-link flex flex-col items-center" style={{ width: '70px' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
+              <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
+            </svg>
+            <span className="text-sm">Home</span>
+          </Link>
+
+          <Link href="/bookings" className="nav-link flex flex-col items-center" style={{ width: '70px' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar-event" viewBox="0 0 16 16">
+              <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z" />
+              <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
+            </svg>
+            <span className="text-sm">Bookings</span>
+          </Link>
+
+          <Link href="/mydata" className="nav-link flex flex-col items-center" style={{ width: '70px' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-card-list" viewBox="0 0 16 16">
+              <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+              <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8m0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0M4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
+            </svg>
+            <span className="text-sm">My Data</span>
+          </Link>
         </footer>
+
       </div>
     </div>
   );
